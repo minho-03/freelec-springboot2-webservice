d19f36281bfe006323d5fb9ee4f62fa80f41fe5f
minho <pmh9038@naver.com>
2026-04-28 15:26:41 +0900

    [Day02-practice] feat: GET /api/v1/posts 엔드포인트 추가

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -7,6 +7,8 @@ import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
@@ -35,6 +37,11 @@ public class PostsApiController {
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
         return id;
+    }
+
+    @GetMapping("/api/v1/posts")
+    public List<PostsResponseDto> findAll(){
+        return postsService.findAllDesc();
     }
 
 }
