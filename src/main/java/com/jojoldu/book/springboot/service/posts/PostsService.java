7e074e96dead0a626af63602f4e901dd67b1053c
minho <pmh9038@naver.com>
2026-04-30 10:02:04 +0900

    day03

--- src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/posts/PostsService.java
@@ -2,19 +2,23 @@ package com.jojoldu.book.springboot.service.posts;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+
 @Service
 public class PostsService {
-
     private final PostsRepository postsRepository;
-
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
@@ -27,7 +31,14 @@ public class PostsService {
                         "해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());  // ⭐ 더티 체킹
         return id;
-
+    }
+    @Transactional
+    public Long delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+        return id;
     }
 
     @Transactional(readOnly = true)
@@ -38,12 +49,10 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
-    @Transactional
-    public void delete(Long id) {
-        Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException(
-                        "해당 게시글이 없습니다. id=" + id));
-        postsRepository.delete(posts);
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findALLDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
-
 }
